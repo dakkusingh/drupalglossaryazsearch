c68ad05d084c2e9b1302a03d204e50f9f7dea09c
Dakku <dakku@97634.no-reply.drupal.org>
2016-04-20 19:47:25 +0100

    new updates - field and widget working on entity fields

---
+++ src/Plugin/Field/FieldType/GlossaryAZItem.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Drupal\search_api_glossary\Plugin\Field\FieldType;
+
+use Drupal\Component\Utility\Random;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Plugin implementation of the 'glossaryaz' field type.
+ *
+ * @FieldType(
+ *   id = "glossary_az",
+ *   label = @Translation("Glossary AZ"),
+ *   module = "search_api_glossary",
+ *   default_widget = "glossary_az",
+ *   description = @Translation("An entity field containing a Glossary AZ.") * )
+ */
+class GlossaryAZItem extends FieldItemBase {
+  /**
+   * {@inheritdoc}
+   */
+  /*public static function defaultStorageSettings() {
+    return array(
+      'max_length' => 255,
+      'is_ascii' => FALSE,
+      'case_sensitive' => FALSE,
+    ) + parent::defaultStorageSettings();
+  }*/
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    // Prevent early t() calls by using the TranslatableMarkup.
+    $properties['value'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Glossary AZ Value'))
+      ->setRequired(FALSE);
+
+    // TODO: Lock this field to prevent editing
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    $schema = array(
+      'columns' => array(
+        'value' => array(
+          'type' => 'text',
+          'size' => 'tiny',
+          'not null' => FALSE,
+        ),
+      ),
+    );
+
+    return $schema;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  /*public function getConstraints() {
+    $constraints = parent::getConstraints();
+
+    if ($max_length = $this->getSetting('max_length')) {
+      $constraint_manager = \Drupal::typedDataManager()->getValidationConstraintManager();
+      $constraints[] = $constraint_manager->create('ComplexData', array(
+        'value' => array(
+          'Length' => array(
+            'max' => $max_length,
+            'maxMessage' => t('%name: may not be longer than @max characters.', array(
+              '%name' => $this->getFieldDefinition()->getLabel(),
+              '@max' => $max_length
+            )),
+          ),
+        ),
+      ));
+    }
+
+    return $constraints;
+  }*/
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function generateSampleValue(FieldDefinitionInterface $field_definition) {
+    $random = new Random();
+    $values['value'] = $random->word(mt_rand(1, 1));
+    return $values;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  /*public function storageSettingsForm(array &$form, FormStateInterface $form_state, $has_data) {
+    $elements = [];
+
+    $elements['max_length'] = array(
+      '#type' => 'number',
+      '#title' => t('Maximum length'),
+      '#default_value' => $this->getSetting('max_length'),
+      '#required' => TRUE,
+      '#description' => t('The maximum length of the field in characters.'),
+      '#min' => 1,
+      '#disabled' => $has_data,
+    );
+
+    return $elements;
+  }*/
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    $value = $this->get('value')->getValue();
+    return $value === NULL || $value === '';
+  }
+
+
+  //TODO
+  /**
+   * {@inheritdoc}
+   */
+  /*public function preSave() {
+    //parent::preSave();
+
+    ksm($this);
+  }*/
+}
---
+++ src/Plugin/Field/FieldWidget/GlossaryAZWidget.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Drupal\search_api_glossary\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'glossary_az' widget.
+ *
+ * @FieldWidget(
+ *   id = "glossary_az",
+ *   label = @Translation("Glossary AZ Widget"),
+ *   field_types = {
+ *     "glossary_az"
+ *   }
+ * )
+ */
+class GlossaryAZWidget extends WidgetBase {
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return array(
+      'size' => 60,
+      'placeholder' => '',
+    ) + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $elements = [];
+
+    $elements['size'] = array(
+      '#type' => 'number',
+      '#title' => t('Size of textfield'),
+      '#default_value' => $this->getSetting('size'),
+      '#required' => TRUE,
+      '#min' => 1,
+    );
+    $elements['placeholder'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Placeholder'),
+      '#default_value' => $this->getSetting('placeholder'),
+      '#description' => t('Text that will be shown inside the field until a value is entered. This hint is usually a sample value or a brief description of the expected format.'),
+    );
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+
+    $summary[] = t('Textfield size: !size', array('!size' => $this->getSetting('size')));
+    if (!empty($this->getSetting('placeholder'))) {
+      $summary[] = t('Placeholder: @placeholder', array('@placeholder' => $this->getSetting('placeholder')));
+    }
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element = [];
+
+    $element['value'] = $element + array(
+      '#type' => 'hidden',
+      '#default_value' => isset($items[$delta]->value) ? $items[$delta]->value : NULL,
+      '#size' => $this->getSetting('size'),
+      '#placeholder' => $this->getSetting('placeholder'),
+      '#maxlength' => $this->getFieldSetting('max_length'),
+      '#element_validate' => array(
+        array($this, 'validate'),
+      ),
+    );
+
+    return $element;
+  }
+
+  /**
+   * Validate the Glossary Widget
+   */
+  public function validate($element, FormStateInterface $form_state) {
+    $value = $element['#value'];
+    $source_value = $form_state->getFormObject()->getEntity()->get('title')->value;
+
+    // TODO Add functions to get Glossary letter
+    //if (strlen($value) == 0) {
+      $form_state->setValueForElement($element, $source_value);
+      return;
+    //}
+
+  }
+
+}
