1bb866add49c001fc921b979ecb9fc61a4d282b2
Dakku <dakku@97634.no-reply.drupal.org>
2016-04-23 12:29:26 +0100

    lets add a form for sorting order

--- src/Plugin/facets/processor/GlossaryAZAllItemsProcessor.php
+++ src/Plugin/facets/processor/GlossaryAZAllItemsProcessor.php
@@ -58,4 +58,11 @@ class GlossaryAZAllItemsProcessor extends ProcessorPluginBase implements BuildPr
     return $results;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    
+  }
+
 }
