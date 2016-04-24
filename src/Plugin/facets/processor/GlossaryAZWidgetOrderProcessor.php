251aefcc719ac4c51bf7389225cfd088d375da8f
Dakku <dakku@97634.no-reply.drupal.org>
2016-04-24 19:07:40 +0100

    cleanup time

--- src/Plugin/facets/processor/GlossaryAZWidgetOrderProcessor.php
+++ src/Plugin/facets/processor/GlossaryAZWidgetOrderProcessor.php
@@ -174,37 +174,4 @@ class GlossaryAZWidgetOrderProcessor extends WidgetOrderPluginBase implements Wi
     return $sort_options_by_weight;
   }
 
-  /**
-   * Sorts default.
-   */
-/*  protected static function sortGlossaryAZDefault(Result $a, Result $b) {
-
-    // TODO Maybe check if the values are present.
-    $a_value = $a->getRawValue();
-    $b_value = $b->getRawValue();
-
-    // TODO make some way to make 0-9 configurable.
-    // so we can have seperate bins for 0,1,2,3 etc
-
-    if ($a_value == $b_value) {
-      return 0;
-    }
-    elseif (ctype_alpha($a_value) && ctype_alpha($b_value)) {
-      return ($a_value < $b_value) ? -1 : 1;
-    }
-    elseif (($a_value == "#" || $a_value == "0-9") && ctype_alpha($b_value)) {
-      return 1;
-    }
-    elseif (ctype_alpha($a_value) && ($b_value == "#" || $b_value == "0-9")) {
-      return -1;
-    }
-    elseif ($a_value == "#" && $b_value == "0-9") {
-      return 1;
-    }
-    elseif ($b_value == "0-9" && $a_value == "#") {
-      return -1;
-    }
-
-  }*/
-
 }
