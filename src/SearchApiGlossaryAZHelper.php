21fe82ece6dc83c83b091d01b16cddd76832ac88
dakku <dakku@97634.no-reply.drupal.org>
2017-04-11 19:32:53 +0100

    cleanup of helper class

--- src/SearchApiGlossaryAZHelper.php
+++ src/SearchApiGlossaryAZHelper.php
@@ -19,13 +19,14 @@ class SearchApiGlossaryAZHelper {
    */
   public function glossaryGetter($source_value, $glossary_az_grouping) {
     $first_letter = strtoupper(substr(trim($source_value), 0, 1));
-    return $this->glossaryGetterHelper($first_letter, array_values($glossary_az_grouping));
+    return self::glossaryGetterHelper($first_letter, array_values($glossary_az_grouping));
   }
 
   /**
    * Getter Helper for Alpha Numeric Keys.
    */
   public function glossaryGetterHelper($first_letter, $glossary_az_grouping) {
+    //ksm(array($first_letter, $glossary_az_grouping));
     // Is it Alpha?
     if (ctype_alpha($first_letter)) {
       // Do we have Alpha grouping?
