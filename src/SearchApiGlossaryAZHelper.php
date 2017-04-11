d9280054a2ed073c8fac75be9b5910177cc02038
dakku <dakku@97634.no-reply.drupal.org>
2017-04-11 12:00:16 +0100

    Adding a new help class to move all helper functions from .module to a class

---
+++ src/SearchApiGlossaryAZHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\search_api_glossary\SearchApiGlossaryAZHelper.
+ */
+
+namespace Drupal\search_api_glossary;
+
+/**
+ * Search Api GlossaryAZ Helper class.
+ *
+ * @package Drupal\search_api_glossary
+ */
+class SearchApiGlossaryAZHelper {
+
+
+}
