582dec3ffeae310e60e3bc1830bba424f3105dfc
dakku <dakku@97634.no-reply.drupal.org>
2017-04-11 13:37:47 +0100

    Moved the helper functions to helper class

--- src/SearchApiGlossaryAZHelper.php
+++ src/SearchApiGlossaryAZHelper.php
@@ -14,5 +14,42 @@ namespace Drupal\search_api_glossary;
  */
 class SearchApiGlossaryAZHelper {
 
+  /**
+   * Getter callback for title_az_glossary property.
+   */
+  public function glossaryGetter($source_value, $glossary_az_grouping) {
+    $first_letter = strtoupper(substr(trim($source_value), 0, 1));
+    return $this->glossaryGetterHelper($first_letter, array_values($glossary_az_grouping));
+  }
+
+  /**
+   * Getter Helper for Alpha Numeric Keys.
+   */
+  public function glossaryGetterHelper($first_letter, $glossary_az_grouping) {
+    // Is it Alpha?
+    if (ctype_alpha($first_letter)) {
+      // Do we have Alpha grouping?
+      if (in_array('glossary_az_grouping_az', $glossary_az_grouping, TRUE)) {
+        $first_letter = "A-Z";
+      }
+      return $first_letter;
+    }
+
+    // Is it a number?
+    elseif (ctype_digit($first_letter)) {
+      // Do we have Numeric grouping?
+      if (in_array('glossary_az_grouping_09', $glossary_az_grouping, TRUE)) {
+        $first_letter = "0-9";
+      }
+      return $first_letter;
+    }
+
+    // Catch non alpha numeric.
+    // Do we have Non Alpha Numeric grouping?
+    elseif (in_array('glossary_az_grouping_other', $glossary_az_grouping, TRUE)) {
+      $first_letter = "#";
+      return $first_letter;
+    }
+  }
 
 }
